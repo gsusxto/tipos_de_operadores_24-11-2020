d8cca162a4e18385cee4ef68254d3f7b569627a2
unknown <=>
2020-11-24 19:25:44 -0500

    tipos_operadores

---
+++ main.js
@@ -0,0 +1,86 @@
+// let nombre_usuario;
+
+// nombre_usuario = prompt("ingrese nombre");
+
+// console.log("el nombre del usuario es: ", nombre_usuario);
+
+// let numero1= 5;
+// let numero2= 15;
+
+// console.log("contenido de la variable 1 y nombre de la variable 1 es: ", numero1,);
+// console.log("contenido de la variable 2 y nombre de la variable 2 es :", numero2,);
+
+// numero1 = numero2;
+
+// console.log("la variable 1 contiene lo que tiene la variable 2: ", numero1);
+
+// console.log("contenido de la variable 1 y nombre de la variable 1 es: ", numero1,);
+// console.log("contenido de la variable 2 y nombre de la variable 2 es :", numero2,);
+
+//operacion de adicion 
+// let numero1= 5;
+// let numero2= 15;
+// let resultado;
+
+// resultado = numero1 + numero2;
+
+// console.log("numero 1 es: ", numero1);
+// console.log("numero 2 es: ", numero2);
+// console.log("suma es: ", resultado);
+
+//operacion de sustraccion  
+// let numero1= 5;
+// let numero2= 15;
+// let resultado;
+
+// resultado = numero1 - numero2;
+
+// console.log("numero 1 es: ", numero1);
+// console.log("numero 2 es: ", numero2);
+// console.log("resta es: ", resultado);
+
+//operacion de multiplicacion
+// let numero1= 5;
+// let numero2= 15;
+// let resultado;
+
+// resultado = numero1 * numero2;
+
+// console.log("numero 1 es: ", numero1);
+// console.log("numero 2 es: ", numero2);
+// console.log("la multiplicacion es: ", resultado);
+
+//operacion de division
+// let numero1= 25;
+// let numero2= 5;
+// let resultado;
+
+// resultado = numero1 / numero2;
+
+// console.log("numero 1 es: ", numero1);
+// console.log("numero 2 es: ", numero2);
+// console.log("la multiplicacion es: ", resultado);
+
+//operacion de resto
+// let numero1= 25;
+// let numero2= 5;
+// let resultado;
+
+// resultado = numero1 % numero2;
+
+// console.log("numero 1 es: ", numero1);
+// console.log("numero 2 es: ", numero2);
+// console.log("el resto es: ", resultado);
+
+//operacion de exponente
+let numero1= 2;
+let numero2= 8;
+let resultado;
+
+resultado = numero1 ** numero2;
+
+console.log("numero 1 es: ", numero1);
+console.log("numero 2 es: ", numero2);
+console.log("el resto es: ", resultado);
+
+
